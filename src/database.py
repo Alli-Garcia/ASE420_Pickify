d4a4001bdbcdd00059069ec1da46747f9b9a3da6
Alli <alligarcia2021@gmail.com>
2024-12-01 15:01:34 -0500

    updating .env

--- src/database.py
+++ src/database.py
@@ -2,15 +2,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
 import os
 
-# MongoDB connection setup
-MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")  # Username environment variable
-MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")  # Password environment variable
-
-if not MONGODB_USERNAME or not MONGODB_PASSWORD:
-    raise ValueError("MONGODB_USERNAME or MONGODB_PASSWORD environment variables are not set.")
-
-MONGODB_URI = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@pickify.1ozxp.mongodb.net/?retryWrites=true&w=majority&appName=Pickify"
-
+MONGODB_URI = os.getenv("MONGODB_URI")
+if not MONGODB_URI:
+    raise ValueError("MONGODB_URI environment variable is not set.")
 # Initialize Async MongoDB Client
 client = AsyncIOMotorClient(MONGODB_URI)
 database = client["pickify_db"]
@@ -22,8 +16,9 @@ device_tokens_collection = database["device_tokens"]
 # Test the database connection
 async def test_connection():
     try:
-        # Ping the database to check connection
-        await database.command("ping")
+        # Use the admin database to ping MongoDB
+        admin_db = client["admin"]
+        await admin_db.command("ping")
         print("Connected to MongoDB successfully!")
     except Exception as e:
         print(f"Failed to connect to MongoDB: {e}")
